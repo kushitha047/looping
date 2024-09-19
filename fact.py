95ce3ea34babe0eb1424a955004ef8040fc6f3b1
kushitha047 <22hr1a3234@mtieat.org>
2024-09-19 14:06:34 +0530

    Create fact.py

---
+++ fact.py
@@ -0,0 +1,36 @@
+***
+Write a program to determine whether 'n' is a factorial number or not. Factorial of a number is the product of all positive numbers from 1 to 'n'.
+Input format:
+The input containing an integer 'n' which denotes the given number.
+Output format:
+If the given number is factorial, print "Yes". Otherwise, print "No".
+Refer the sample output for formatting.
+Sample Input:
+6
+Sample Output:
+Yes
+***
+import math
+
+def is_factorial_number(n):
+    if n < 1:
+        return "No"
+    
+    # Start with factorial of 1
+    factorial = 1
+    i = 1
+    
+    while factorial < n:
+        i += 1
+        factorial *= i
+        
+    if factorial == n:
+        return "Yes"
+    else:
+        return "No"
+
+# Read the input
+n = int(input())
+
+# Determine and print the result
+print(is_factorial_number(n), "program")
