478e54e57efd469473140fe77ec16bbe8d19c0fa
kushitha047 <22hr1a3234@mtieat.org>
2024-09-19 15:52:45 +0530

    Update club.py

--- club.py
+++ club.py
@@ -1,5 +1,5 @@
 ***
-)The environmental eco club has discovered a new Amoeba that grows in the order of a Fibonacci series every month. They are exhibiting their amoeba in a national conference. They want to know the size of the amoeba at a particular time instant. If a particular month’s index is given, write a  program to displays the amoeba’s size……???. For Example, The size of the amoeba on month 1, 2, 3, 4, 5, 6, ..will be 0, 1, 1, 2, 3, 5, 8 respectively.
+The environmental eco club has discovered a new Amoeba that grows in the order of a Fibonacci series every month. They are exhibiting their amoeba in a national conference. They want to know the size of the amoeba at a particular time instant. If a particular month’s index is given, write a  program to displays the amoeba’s size……???. For Example, The size of the amoeba on month 1, 2, 3, 4, 5, 6, ..will be 0, 1, 1, 2, 3, 5, 8 respectively.
 Input format:
 The first input containing an integer which denotes the number of the month
 Output format:
